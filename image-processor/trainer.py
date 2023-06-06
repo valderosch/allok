6c695010f7e40dd77729ac86e55cd5a3635f9254
valderosch <phonelix96@gmail.com>
2023-06-06 04:38:17 +0300

    before

--- image-processor/trainer.py
+++ image-processor/trainer.py
@@ -1,11 +1,12 @@
 import cv2 as cv
 import pickle
 
-image = "./assets/train-data/trainer-mid.png"
+image = "./assets/train-data/trainer-low.jpg"
 fimg = cv.imread(image)
 
-width = 107
-height = 48
+top = bottom = left = right = 150
+border_color = (255, 255, 255)
+
 
 try:
     with open('park-positions', 'rb') as f:
@@ -13,27 +14,92 @@ try:
 except:
     posList = []
 
-def mouseClick(events, x,y, flags, params):
-    img = cv.imread(image)
-    if events == cv.EVENT_LBUTTONDOWN:
-        posList.append((x, y))
+drawing = False
+start_point = (-1, -1)
+
+def mouseCallback(event, x, y, flags, param):
+    global start_point, drawing
+
+    if event == cv.EVENT_LBUTTONDOWN:
+        drawing = True
+        start_point = (x, y)
+    elif event == cv.EVENT_LBUTTONUP:
+        drawing = False
+        width = x - start_point[0]
+        height = y - start_point[1]
+        posList.append((start_point[0], start_point[1], width, height))
         print(f"{posList}")
-    elif events == cv.EVENT_RBUTTONDOWN:
+
+    elif event == cv.EVENT_RBUTTONDOWN:
         for i, pos in enumerate(posList):
-            x1, y1 = pos
-            if x1 < x < x1 + width and y1 < y < y1 + height:
+            x1, y1, w, h = pos
+            if x1 <= x <= x1 + w and y1 <= y <= y1 + h:
                 posList.pop(i)
                 print(f"{i} + + + {posList}")
 
-    with open('park-positions', 'wb') as f:
-        pickle.dump(posList, f)
-
-
+cv.namedWindow("Trainer")
+cv.setMouseCallback("Trainer", mouseCallback)
 
 while True:
-    for position in posList:
-        cv.rectangle(fimg, position, (position[0]+width, position[1]+height), (255, 50, 255), 1)
+    img = fimg.copy()
 
-    cv.imshow("Trainer", fimg)
-    cv.setMouseCallback("Trainer", mouseClick)
-    cv.waitKey(1)
+    for position in posList:
+        x, y, w, h = position
+        cv.rectangle(img, (x, y), (x + w, y + h), (219, 12, 21), 3)
+
+    cv.imshow("Trainer", img)
+
+    top = bottom = left = right = 150
+    border_color = (255, 255, 255)
+
+    try:
+        with open('coords', 'rb') as f:
+            posList = pickle.load(f)
+    except:
+        posList = []
+
+    drawing = False
+    start_point = (-1, -1)
+
+
+    def mouseCallback(event, x, y, flags, param):
+        global start_point, drawing
+
+        if event == cv.EVENT_LBUTTONDOWN:
+            drawing = True
+            start_point = (x, y)
+        elif event == cv.EVENT_LBUTTONUP:
+            drawing = False
+            width = x - start_point[0]
+            height = y - start_point[1]
+            posList.append((start_point[0], start_point[1], width, height))
+            print(f"{posList}")
+
+        elif event == cv.EVENT_RBUTTONDOWN:
+            for i, pos in enumerate(posList):
+                x1, y1, w, h = pos
+                if x1 <= x <= x1 + w and y1 <= y <= y1 + h:
+                    posList.pop(i)
+                    print(f"{i} + + + {posList}")
+
+
+    cv.namedWindow("Trainer")
+    cv.setMouseCallback("Trainer", mouseCallback)
+
+    while True:
+        img = fimg.copy()
+
+        for position in posList:
+            x, y, w, h = position
+            cv.rectangle(img, (x, y), (x + w, y + h), (219, 12, 21), 3)
+
+        cv.imshow("Trainer", img)
+
+        # LANG ! Eng
+        key = cv.waitKey(1)
+        if key == ord('q'):
+            with open('coords', 'wb') as f:
+                pickle.dump(posList, f)
+            break
+
+    cv.destroyAllWindows()
--- image-processor/visor.py
+++ image-processor/visor.py
@@ -1,85 +1,87 @@
 import cv2
-# Allok app v 0.1
-
 import pickle
-# import cvzone
 import numpy as np
+import cvzone
 
-img = "./assets/train-data/trainer-mid.png"
-cap = cv2.VideoCapture('trainer.mp4')
-width = 103
-height = 43
-with open('park-positions', 'rb') as f:
-    posList = pickle.load(f)
+font = cv2.FONT_HERSHEY_DUPLEX
+font_color = (9, 255, 25)
+font_scale = 1
+font_org = (20, 50)
+VIDEOPATH = "./assets/train-data/trainer.mp4"
+IMGPATH = "./assets/train-data/trainer-low.jpg"
+top = bottom = left = right = 150
+border_color = (255, 255, 255)
+# Video feed
+# cap = cv2.VideoCapture(IMAGEPATH)
+
+x_offset = left
+y_offset = top
+
+# Розпакування позицій
+with open('coords', 'rb') as image:
+    posList = pickle.load(image)
+
+# Отримання значень ширини і висоти
+width = posList[0][2] - posList[0][0]
+height = posList[0][3] - posList[0][1]
+
+shifted_posList = [(x + x_offset, y + y_offset, x + width + x_offset, y + height + y_offset) for x, y, width, height in posList]
 
 
-def empty(a):
-    pass
+
+def checkParkingSpace(img, imgPro):
+    spaceCounter = 0
+    image_with_border = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=border_color)
+
+    for pos in shifted_posList:
+        x, y, w, h = pos
+        box_color = None
+        box_thickness = None
+        count = 0
+        if x >= 0 and y >= 0 and x + w <= img.shape[1] and y + h <= img.shape[0]:
+            imgCrop = imgPro[y:y + h, x:x + w]
+            cv2.imshow(str(x * y), imgCrop)
+            count = cv2.countNonZero(imgCrop)
 
 
-cv2.namedWindow("Vals")
-cv2.resizeWindow("Vals", 640, 240)
-cv2.createTrackbar("Val1", "Vals", 25, 50, empty)
-cv2.createTrackbar("Val2", "Vals", 16, 50, empty)
-cv2.createTrackbar("Val3", "Vals", 5, 50, empty)
-
-
-def checkSpaces():
-    spaces = 0
-    for pos in posList:
-        x, y = pos
-        w, h = width, height
-
-        imgCrop = imgThres[y:y + h, x:x + w]
-        count = cv2.countNonZero(imgCrop)
-
-        if count < 900:
-            color = (0, 200, 0)
-            thic = 5
-            spaces += 1
-
+            if count < 900:
+                box_color = (0, 255, 0)
+                box_thickness = 5
+                spaceCounter += 1
+            else:
+                box_color = (0, 0, 255)
+                box_thickness = 2
         else:
-            color = (0, 0, 200)
-            thic = 2
-
-        cv2.rectangle(img, (x, y), (x + w, y + h), color, thic)
-
-        cv2.putText(img, str(cv2.countNonZero(imgCrop)), (x, y + h - 6), cv2.FONT_HERSHEY_PLAIN, 1,
-                    color, 2)
-
-    # cvzone.putTextRect(img, f'Free space count: {spaces}/{len(posList)}', (50, 60), thickness=3, offset=20,
-    #                    colorR=(0, 200, 0))
-    print(f'Free space count: {spaces}/{len(posList)}')
+            print("Помилка: Некоректні координати для обрізки зображення")
 
 
-while True:
-    # Get image frame
-    success, img = cap.read()
-    # loop current video if it ends
-    if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
-        cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
-    # img = cv2.imread('img.png')
-    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    imgBlur = cv2.GaussianBlur(imgGray, (3, 3), 1)
-    # ret, imgThres = cv2.threshold(imgBlur, 150, 255, cv2.THRESH_BINARY)
+        # load status calculation
+        if spaceCounter < len(posList) / 3:
+            loadstatus = 'heavy'
+        elif spaceCounter < len(posList) / 1.4 and len(posList) > len(posList) / 3:
+            loadstatus = 'medium'
+        elif spaceCounter > len(posList) / 1.4:
+            loadstatus = 'light'
+        font_box_org = (x, y + height - 5)
+        x, y, x2, y2 = pos
+        cv2.rectangle(image_with_border, (x, y), (x2, y2), box_color, box_thickness)
+        cv2.putText(image_with_border, str(count), font_box_org , fontFace=font, fontScale=0.5, color=(255, 255, 255), thickness=1)
+    cv2.rectangle(image_with_border, (10, 70), (1080, 10), (0, 0, 0), -1)
+    cv2.putText(image_with_border, f'Camera #00 / Free spaces: [{spaceCounter}] of [{len(posList)}] / Load: {loadstatus} load',
+                font_org, font, fontScale=font_scale, color = font_color, thickness=1)
+    print(f"Camera #00: \nSpaces: \nAll:[{len(posList)}] \nFree:[{spaceCounter}] \nUsed: [{len(posList) - spaceCounter}] \n"
+          f"Load status: {loadstatus}")
 
-    val1 = cv2.getTrackbarPos("Val1", "Vals")
-    val2 = cv2.getTrackbarPos("Val2", "Vals")
-    val3 = cv2.getTrackbarPos("Val3", "Vals")
-    if val1 % 2 == 0: val1 += 1
-    if val3 % 2 == 0: val3 += 1
-    imgThres = cv2.adaptiveThreshold(imgBlur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-                                     cv2.THRESH_BINARY_INV, val1, val2)
-    imgThres = cv2.medianBlur(imgThres, val3)
-    kernel = np.ones((3, 3), np.uint8)
-    imgThres = cv2.dilate(imgThres, kernel, iterations=1)
+img = cv2.imread(IMGPATH)
+image_with_border = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=border_color)
+imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+imgBlur = cv2.GaussianBlur(imgGray, (3, 3), 1)
+imgThreshold = cv2.adaptiveThreshold(imgBlur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 25, 16)
+imgMedian = cv2.medianBlur(imgThreshold, 5)
+kernel = np.ones((3, 3), np.uint8)
+imgDilate = cv2.dilate(imgMedian, kernel, iterations=1)
 
-    checkSpaces()
-    # Display Output
-
-    cv2.imshow("Trainer", img)
-    # cv2.imshow("ImageGray", imgThres)
-    # cv2.imshow("ImageBlur", imgBlur)
-    key = cv2.waitKey(1)
-    if key == ord('r'):
-        pass
+checkParkingSpace(img, imgDilate)
+cv2.imshow("Image with Borders", image_with_border)
+key = cv2.waitKey(0)
+cv2.destroyAllWindows()
